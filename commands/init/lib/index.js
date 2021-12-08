1e8db339f5b0a6b5f1061bc51f02fd1965b07834
yzy9527 <cyzy9527@163.com>
2021-12-08 23:30:06 +0800

    feature:自定义模板安装

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -158,7 +158,26 @@ class InitCommand extends Command {
     }
 
     async installCustomTemplate() {
-        console.log('自定义安装');
+        //自定义安装'
+        if (await this.templateNpm.exists()) {
+            const rootFile = this.templateNpm.getRootFilePath();
+            if (fs.existsSync(rootFile)) {
+                log.notice('开始执行自定义模板');
+                const templatePath = path.resolve(this.templateNpm.cacheFilePath, 'template');
+                const options = {
+                    templateInfo: this.templateInfo,
+                    projectInfo: this.projectInfo,
+                    sourcePath: templatePath,
+                    targetPath: process.cwd()
+                };
+                const code = `require('${rootFile}')(${JSON.stringify(options)})`;
+                await execAsync('node', ['-e', code], {stdio: 'inherit', cwd: process.cwd()});
+                log.success('自定义模板安装成功');
+            } else {
+                throw new Error('自定义入口文件不存在！');
+            }
+        }
+
     }
 
     async downloadTemplate() {
